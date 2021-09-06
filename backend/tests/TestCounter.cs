33e9124b7d6bdb46106bae043b8c5a1487c21c5e
mundavak <mundavake@gmail.com>
2021-09-05 21:15:57 -0500

    updates to main.js and index.html

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -4,7 +4,6 @@ using Xunit;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
@@ -17,13 +16,13 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public void Http_trigger_should_return_known_string()
         {
             var counter = new Company.Function.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
+            var response = (HttpResponseMessage)Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
             Assert.Equal(3, counter.Count);
         }
 
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContendloaded', (event) =>{
     getVisitCount();
-})
+});
 
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
@@ -8,7 +8,8 @@ const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
         return response.json
-    }).then(response =>{
+    })
+    .then(response =>{
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
