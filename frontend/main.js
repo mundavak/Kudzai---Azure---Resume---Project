3917cd4c79052a5e8fd9782a2eb829edb96b76f9
mundavak <mundavak@gmail.com>
2021-09-13 14:30:43 -0500

    Updated main.js fetch

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContendloaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
-});
+})
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://kudzairesumecounter.azurewebsites.net/api/GetResumeCounter?code=9npAfaIk99CjIPY78qEP9u09qdtnQBQFNlNeGzchkrQikiMnwVGlCw==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json
-    })
-    .then(response =>{
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
         console.log("Website called function API.");
-        count = response.count;
+        count =  response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
